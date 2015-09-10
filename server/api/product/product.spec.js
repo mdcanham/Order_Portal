cf9a7fcd5d9335998cee9e6d1b6faff1095adf1c
Matthew Canham <mdcanham@me.com>
2015-09-11 04:04:49 +1200

    Updated test and minor bug fix based on that test.

--- server/api/product/product.spec.js
+++ server/api/product/product.spec.js
@@ -9,11 +9,10 @@ describe('GET /api/products', function() {
   it('should respond with JSON array', function(done) {
     request(app)
       .get('/api/products')
-      .expect(200)
-      .expect('Content-Type', /json/)
+      .expect(401)
+      .expect('Content-Type', /text/)
       .end(function(err, res) {
         if (err) return done(err);
-        res.body.should.be.instanceof(Array);
         done();
       });
   });
